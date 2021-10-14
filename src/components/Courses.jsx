03fcc05f1b24e0cd08518ebbe656724a95a82764
Robin <robinbtech17@gmail.com>
2021-10-14 12:09:53 +0530

    Adding Usecontext hook.

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,31 +1,28 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
-export class Courses extends Component {
-  static contextType = ThemeContext;
+export const Courses = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
 
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div
-        className="courses"
-        style={{ backgroundColor: theme.bg, color: theme.color }}
-      >
-        <ul className="course-list">
-          <li className="course-item" style={{ background: theme.ui }}>
-            React JS
-          </li>
-          <li className="course-item" style={{ background: theme.ui }}>
-            Node JS
-          </li>
-          <li className="course-item" style={{ background: theme.ui }}>
-            Javascript
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      className="courses"
+      style={{ backgroundColor: theme.bg, color: theme.color }}
+    >
+      <ul className="course-list">
+        <li className="course-item" style={{ background: theme.ui }}>
+          React JS
+        </li>
+        <li className="course-item" style={{ background: theme.ui }}>
+          Node JS
+        </li>
+        <li className="course-item" style={{ background: theme.ui }}>
+          Javascript
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Courses;
